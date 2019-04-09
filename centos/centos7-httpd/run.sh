61813105f2aee1a436fa62932ae155343a77dad7
sunnianjun <sunnianjun@huobi.com>
2019-04-09 16:18:37 +0800

    Refactor: after update the systemd base, still doesn't work

--- centos/centos7-httpd/run.sh
+++ centos/centos7-httpd/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -ti -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged=true -p 18080:80 taojintianxia/centos7-httpd
+docker run -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged=true -p 18080:80 taojintianxia/centos7-httpd
