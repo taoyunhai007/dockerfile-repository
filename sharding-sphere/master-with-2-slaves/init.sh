ffbf60ccbdb44d6e40600e7552284408c3421bfb
sunnianjun <sunnianjun@huobi.com>
2019-08-07 11:39:24 +0800

    Add: add new folder

--- sharding-sphere/master-with-2-slaves/init.sh
+++ sharding-sphere/master-with-2-slaves/init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget -N https://raw.githubusercontent.com/apache/incubator-shardingsphere-example/dev/src/resources/manual_schema.sql
+wget -N -P sql/ https://raw.githubusercontent.com/apache/incubator-shardingsphere-example/dev/src/resources/manual_schema.sql
