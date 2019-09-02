e3f76ca54e78283e7017546b8a48ec6965ca97d4
sunnianjun <sunnianjun@huobi.com>
2019-09-02 14:26:58 +0800

    Refactor: update the resource sql

--- sharding-sphere/resources/manual_schema.sql
+++ sharding-sphere/resources/manual_schema.sql
@@ -29,6 +29,8 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_0;
 CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
 
 CREATE TABLE IF NOT EXISTS demo_ds_master.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
+CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
+CREATE TABLE IF NOT EXISTS demo_ds_slave_1.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
 
 -- Should sync from master-salve automatically
 -- CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
