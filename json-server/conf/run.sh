bf10e00d47634409cb190c3106486c8878e5951b
sunnianjun <sunnianjun@huobi.com>
2019-12-11 17:11:33 +0800

    Add: add json-server

---
+++ json-server/conf/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+args="$@"
+
+args="$@ -p 80"
+
+file=/data/db.json
+if [ -f $file ]; then
+    echo "Found db.json, trying to open"
+    args="$args --watch db.json"
+fi
+
+file=/data/file.js
+if [ -f $file ]; then
+    echo "Found file.js seed file, trying to open"
+    args="$args file.js"
+fi
+
+file=/data/routes.json
+if [ -f $file ]; then
+    echo "Found routes.json, trying to open"
+    args="$args --routes routes.json"
+fi
+
+json-server $args
